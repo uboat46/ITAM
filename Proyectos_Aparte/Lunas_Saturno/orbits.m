8cd1604ee8b6ee082e0e0f21b0398eede2976e6b
EMILIANO SOTOMAYOR GONZALEZ <esotomayg@itam.mx>
2017-09-11 12:50:06 -0500

    modularized moons patterns

--- Proyectos_Aparte/Lunas_Saturno/orbits.m
+++ Proyectos_Aparte/Lunas_Saturno/orbits.m
@@ -9,4 +9,25 @@ orbit = [15520 0.942422;
           3561300 79.33018
           12952000 550.48];
 
-twoMoonOrbit(orbit(3,1), orbit(3,2), orbit(5,1), orbit(5,2), 720);
+      m1 = 1;
+      m2 = 9;
+      t = 8000;
+      vel = 0.2;
+twoMoonOrbit(orbit(m1,1), orbit(m1,2), orbit(m2,1), orbit(m2,2), t, vel);
+
+% for i=1:length(orbit)
+%     for j=1:length(orbit)
+%         if ~(i == j)
+%             if(i > j)
+%                 sprintf('i: %d, j: %d = %d',i,j,orbit(i,2)/orbit(j,2))
+%             else
+%                 sprintf('i: %d, j: %d = %d',i,j,orbit(j,2)/orbit(i,2))
+%             end
+%         end
+%     end
+% end
+% RELACIONES CERCANAS A 6
+% 1 : 9
+% 2 : 8
+% 4 : 6
+% 8 : 9
